2f60db9f32947ad3d296449e967d60cca59042d9
Naomi <orangenaomic@gmail.com>
2018-03-06 16:33:16 -0500

    3/6 classwork

--- tron-nc01.py
+++ tron-nc01.py
@@ -1,1 +1,11 @@
 "Naomi's Working Space"
+import pygame
+from pygame.locals import *
+import time
+
+class grid():
+    """creates the grid in which the game is played"""
+    def __init__(self):
+
+
+class cell():
