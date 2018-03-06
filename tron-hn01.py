61cb44ce4fc69712947348acb58d68ee71d21a59
hadleigh-000 <hadleigh.nunes@students.olin.edu>
2018-03-06 15:12:16 -0500

    Tried to add cell class

--- tron-hn01.py
+++ tron-hn01.py
@@ -8,3 +8,22 @@ pygame.init()
 fpsClock = pygame.time.Clock()
 
 WindowSurfaceObj = pygame.display.setmode((640,480))
+
+
+class Cell(object):
+
+    def __init__(self, height, x, y, flip = False):
+        self.height = height
+        self.x = x
+        self.y = y
+        self.flip = flip
+
+
+    def __str__(self):
+        return "Cell height=%f, x=%f, y=%f" % (self.height, self.x, self.y)
+
+
+
+
+class Grid(object):
+    """Defines a grid of cell objects"""
