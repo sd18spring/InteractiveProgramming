b4265e8a6b36d4d1f5cd38ead213b344c3a6bab8
cusaitech <cusaitech@gmail.com>
2018-03-07 18:26:20 -0500

    Create Boundary Object

--- Arena.py
+++ Arena.py
@@ -1,1 +1,14 @@
 #This will be the boundary in which our snake navigates
+
+class Boundary(object):
+    """
+    Defines a boundary object to confine the snake
+
+    Takes attributes height, width, padding
+    padding refers to wall thickness
+    """
+
+    def __init__(self, height, width, padding = 50):
+        self.height = height
+        self.width = width
+        self.padding = padding
