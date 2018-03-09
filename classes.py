6a04ff6ab811fcbd8af5b515d26a63f9a765f66a
Ben Weissman <bweissman1@babson.edu>
2018-03-09 14:42:30 -0800

    Clickable text box in progress

--- classes.py
+++ classes.py
@@ -1,45 +1,156 @@
 import pygame
 import time
-
+import math
 
 class Model(object):
 
-    def __init__(self, size):
+    def __init__(self, size, boxes=None):
         self.size = size
+        self.width = size[0]
+        self.height = size[1]
         self.nodes = []
         self.nodes.append(Node('title',size[0]/2,size[1]/2))
-        self.nodes.append(Node('title2',10,0))
+        self.nodes.append(Node('title2',15,30))
         self.clines = []
         self.clines.append(ConnectionLine(self.nodes[0],self.nodes[1]))
+        self.panning = False
+        self.mouse_pos = None
+        self.boxes = []
+        self.boxes.append(Box('Main Box'))
+        self.rectangle = pygame.Rect(((size[0]/2)-(size[0]/4),(size[1]*.33)-(size[1]/30),(size[0]/2),(size[1]/10)))
 
+    def zoom_in(self,center):
+        """Zooms in around the center
+        center: tuple containing the center about which the screen will
+        be dilated"""
+        for node in self.nodes:
+            node.x = (node.x - center[0])*1.05 + center[0]
+            node.y = (node.y - center[1])*1.05 + center[1]
+        for cline in self.clines:
+            cline.update()
+
+    def zoom_out(self,center):
+        """Zooms out around the center
+        center: tuple containing the center about which the screen will
+        be dilated"""
+        for node in self.nodes:
+            node.x = (node.x - center[0])*0.95 + center[0]
+            node.y = (node.y - center[1])*0.95 + center[1]
+        for cline in self.clines:
+            cline.update()
+
+    def pan(self,dx,dy):
+        """Moves everything on the screen by dx,dy
+        dx: movement in the x direction
+        dy: movement in the y direction"""
+        for node in self.nodes:
+            node.x = node.x + dx
+            node.y = node.y + dy
+        for cline in self.clines:
+            cline.update()
 
 class Viewer(object):
+    """Displays the model"""
 
     def __init__(self,model):
         self.model = model
         self.screen = pygame.display.set_mode(self.model.size)
 
     def draw(self):
-        self.screen.fill = pygame.Color(0,0,0)
+        self.screen.fill(pygame.Color(28, 172, 229))
+        for cline in self.model.clines:
+            pygame.draw.lines(self.screen, pygame.Color(200, 200, 200), False, cline.points,
+                        ConnectionLine.line_width)
         for node in self.model.nodes:
-            pygame.draw.circle(self.screen,pygame.Color(127,127,63),(int(node.x),int(node.y)),node.node_size,10)
+            pygame.draw.circle(self.screen, pygame.Color(175,175,175),
+                            (int(node.x), int(node.y)), node.size,0)
+        for box in self.model.boxes:
+            pygame.draw.rect(self.screen,pygame.Color(255,255,255),pygame.Rect(((self.model.width/2)-(self.model.width/4),(self.model.height*.33)-(self.model.height/30),(self.model.width/2),(self.model.height/10))))
+        pygame.display.update()
+
+class Controler(object):
+    """Handles user input into the model"""
+
+    def __init__(self, model):
+        self.model = model
+
+    def handle_event(self, event):
+        """Updates model according to type of input"""
+        if model.panning:
+            dx = pygame.mouse.get_pos()[0] - self.model.mouse_pos[0]
+            dy = pygame.mouse.get_pos()[1] - self.model.mouse_pos[1]
+            self.model.pan(dx,dy)
+            self.model.mouse_pos = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 5:
+                m_pos = pygame.mouse.get_pos()
+                self.model.zoom_in(m_pos)
+
+            elif event.button == 4:
+                m_pos = pygame.mouse.get_pos()
+                self.model.zoom_out(m_pos)
+
+            elif event.button == 1:
+                rect = self.model.rectangle
+                self.model.mouse_pos = pygame.mouse.get_pos()
+                print(self.model.mouse_pos)
+
+            elif pygame.mouse.get_pressed()[0]:
+                rect = self.model.rectangle
+                m_pos = pygame.mouse.get_pos()
+                if not (rect[0] < m_pos[0] < rect[0]+rect[2] and rect[1] < m_pos[1] < rect[1]+rect[3]):
+                    self.model.panning = True
+                    self.model.mouse_pos = pygame.mouse.get_pos()
+
+            elif pygame.mouse.get_pressed()[0] == False:
+                self.model.panning = False
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.model.pan(0,10)
+
+            if event.key == pygame.K_DOWN:
+                self.model.pan(0,-10)
+
+            if event.key == pygame.K_LEFT:
+                self.model.pan(10,-0)
+
+            if event.key == pygame.K_RIGHT:
+                self.model.pan(-10,0)
+
+class Box(object):
+    """A clickable box where the user enters the title of the page she/he is
+    interested in"""
+
+    def __init__(self,title=''):
+        self.title = title
+
+    def __str__(self):
+        return '%s at (%d,%d)' % (title,x,y)
 
 class Node(object):
+    """A clickable node appearing in a web
+    Attributes: x, y, title, children (list of the titles of nodes linked to
+    by the node)"""
 
     node_size = 10
 
     def __init__(self,title,x,y):
+        self.children = []
         self.x = x
         self.y = y
-        self.size = 10
+        self.title = title
+        self.size = Node.node_size
 
     def __str__(self):
         return '%d,%d' % (self.x,self.y)
 
-
 class ConnectionLine(object):
 
     line_length = 10
+    line_width = 3
 
     def __init__(self,start,end):
         """Start and end are nodes"""
@@ -49,27 +160,45 @@ class ConnectionLine(object):
         self.y0 = start.y
         self.x1 = end.x
         self.y1 = end.y
+        self.points = [(self.x0,self.y0), (self.x1, self.y1)]
 
     def __str__(self):
         return 'Start: %s   End: %s'  % (str(self.start),str(self.end))
 
-
+    def update(self):
+        """Recalculates the line endpoints when nodes are changed"""
+        self.x0 = self.start.x
+        self.y0 = self.start.y
+        self.x1 = self.end.x
+        self.y1 = self.end.y
+        self.points = [(self.x0,self.y0), (self.x1, self.y1)]
 
 if __name__ == '__main__':
+
     pygame.init()
 
     node1 = Node('Node1',1,2)
     node2 = Node('Node2',3,4)
 
+
+
     running = True
 
-    view = Viewer(Model((600,400)))
-    view.draw()
+    model = Model((1000,1000))
+
+
+    view = Viewer(model)
+    controler = Controler(model)
+    #view.draw()
     k=0
-    while k<10 and running == True:
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                break
+            controler.handle_event(event)
+
             view.draw()
             time.sleep(.001)
-            k=k+1
+
+    pygame.quit()
