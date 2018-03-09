bf1234944e2fc5a74e64e9b1460617345eb369e0
nathanestill <nathan.estill@students.olin.edu>
2018-03-09 15:22:33 -0500

    deleting black screen and adding CollectionOfAsteroids

--- Asteroid.py
+++ Asteroid.py
@@ -26,81 +26,109 @@ class Asteroid():
                 self.y = 0 - self.h
             elif(self.y <= 0 - self.h):
                 self.y = height
+            self.rect = pygame.Rect((self.x,self.y),(self.x+self.w,self.y+self.h))
             gameDisplay.blit(self.image,(self.x,self.y)) # draws the asteroid on the screen
 class LargeAsteroid(Asteroid):
     def __init__(self,x,y,speed,direction):
         super().__init__(x,y,speed,direction)
         self.image = pygame.transform.scale(self.image,(self.w // 2,self.h // 2)) # scales the asteroid to size
         self.w,self.h = self.image.get_size()
+        self.rect = pygame.Rect((self.x,self.y),(self.x+self.w,self.y+self.h))
     def destroy(self):
-        self.destroyed = True
-        MedAster = []
-        for i in range(2):
-            MedAster.append(MediumAsteroid(self.x,self.y,self.speed*2,random.uniform(0,2*math.pi))) #makes two more medium asteroids in it's place with random directions
-        return MedAster
+        if(not self.destroyed):
+            self.destroyed = True
+            MedAster = []
+            for i in range(2):
+                MedAster.append(MediumAsteroid(self.x,self.y,self.speed*1.5,random.uniform(0,2*math.pi))) #makes two more medium asteroids in it's place with random directions
+            return MedAster
+        return None
 class MediumAsteroid(Asteroid):
     def __init__(self,x,y,speed,direction):
         super().__init__(x,y,speed,direction)
         self.image = pygame.transform.scale(self.image,(self.w // 4,self.h // 4)) # half as big as large asteroid
         self.w,self.h = self.image.get_size()
+        self.rect = pygame.Rect((self.x,self.y),(self.x+self.w,self.y+self.h))
+    def destroy(self):
+        if(not self.destroyed):
+            self.destroyed = True
+            SmallAster = []
+            for i in range(2):
+                SmallAster.append(SmallAsteroid(self.x,self.y,self.speed*1.5,random.uniform(0,2*math.pi))) #makes two more medium asteroids in it's place with random directions
+            return SmallAster
+        return None
 class SmallAsteroid(Asteroid):
     def __init__(self,x,y,speed,direction):
         super().__init__(x,y,speed,direction)
         self.image = pygame.transform.scale(self.image,(self.w // 8,self.h // 8)) # half as big as large asteroid
         self.w,self.h = self.image.get_size()
-
-def spawnAsteroids(numberOfAsteroids,gameDisplay,speed):
-    width, height = gameDisplay.get_size()
-    listOfAsteroids = [] # initializes a list of asteroids to update
-    sampleAsteroid = LargeAsteroid(0,0,0,0) # a sample asteroid to know where to spawn the asteroids in case we change the size later
-    smallArea = 100 # the area that asteroids are to spawn around the the edge
-    for i in range(numberOfAsteroids):
-        side = random.randint(1,4)
-        if(side == 1): # left side of the screen
-            x = random.randint(-sampleAsteroid.w // 2,smallArea - sampleAsteroid.w // 2)
-            y = random.randint(-sampleAsteroid.h // 2,height - sampleAsteroid.h // 2)
-        elif(side == 2): # top side of the screen
-            x = random.randint(-sampleAsteroid.w // 2,width - sampleAsteroid.w // 2)
-            y = random.randint(-sampleAsteroid.w // 2,smallArea - sampleAsteroid.w // 2)
-        elif(side == 3): # right side of the screen
-            x = random.randint(width-smallArea - sampleAsteroid.w // 2,width - sampleAsteroid.w // 2)
-            y = random.randint( -sampleAsteroid.w // 2,height - sampleAsteroid.w // 2)
-        elif(side == 4): # bottom of the screen
-            x = random.randint( -sampleAsteroid.w // 2,width - sampleAsteroid.w // 2)
-            y = random.randint(height-smallArea - sampleAsteroid.w // 2,height - sampleAsteroid.w // 2)
-        direction = random.uniform(0,math.pi * 2) # initiate each asteroid with a random direction
-        listOfAsteroids.append(LargeAsteroid(x,y,speed,direction))
-    return listOfAsteroids
-
+        self.rect = pygame.Rect((self.x,self.y),(self.x+self.w,self.y+self.h))
+    def destroy(self):
+        self.destroyed = True
+class CollectionOfAsteroids():
+    def __init__(self,gameDisplay,speed):
+        self.listOfAsteroids = []
+        self.gameDisplay = gameDisplay
+        self.speed = speed
+    def spawnAsteroids(self,numberOfAsteroids):
+        width, height = gameDisplay.get_size()
+        listOfAsteroids = [] # initializes a list of asteroids to update
+        listOfRects = []
+        sampleAsteroid = LargeAsteroid(0,0,0,0) # a sample asteroid to know where to spawn the asteroids in case we change the size later
+        smallArea = 100 # the area that asteroids are to spawn around the the edge
+        for i in range(numberOfAsteroids):
+            side = random.randint(1,4)
+            if(side == 1): # left side of the screen
+                x = random.randint(-sampleAsteroid.w // 2,smallArea - sampleAsteroid.w // 2)
+                y = random.randint(-sampleAsteroid.h // 2,height - sampleAsteroid.h // 2)
+            elif(side == 2): # top side of the screen
+                x = random.randint(-sampleAsteroid.w // 2,width - sampleAsteroid.w // 2)
+                y = random.randint(-sampleAsteroid.w // 2,smallArea - sampleAsteroid.w // 2)
+            elif(side == 3): # right side of the screen
+                x = random.randint(width-smallArea - sampleAsteroid.w // 2,width - sampleAsteroid.w // 2)
+                y = random.randint(-sampleAsteroid.w // 2,height - sampleAsteroid.w // 2)
+            elif(side == 4): # bottom of the screen
+                x = random.randint(-sampleAsteroid.w // 2,width - sampleAsteroid.w // 2)
+                y = random.randint(height-smallArea - sampleAsteroid.w // 2,height - sampleAsteroid.w // 2)
+            direction = random.uniform(0,math.pi * 2) # initiate each asteroid with a random direction
+            listOfAsteroids.append(LargeAsteroid(x,y,self.speed,direction))
+            listOfRects.append(listOfAsteroids[i].rect)
+        self.listOfAsteroids = listOfAsteroids
+        self.listOfRects = listOfRects
+    def updateAsteroids(self,gameDisplay):
+        listOfRects = []
+        print(pygame.Rect.collidelistall(self.listOfRects[0],self.listOfRects))
+        for i in self.listOfAsteroids:
+            i.update(gameDisplay)
+            listOfRects.append(i.rect)
+        self.listOfRects = listOfRects
+    def destroyAll(self):
+        sizeOfAsteroids = range(len(self.listOfAsteroids))
+        for i in sizeOfAsteroids:
+            newAsteroid = self.listOfAsteroids[i].destroy()
+            if(newAsteroid != None):
+                self.listOfAsteroids += newAsteroid # destroying all of the asteroids making them medium
+        for i in sizeOfAsteroids:
+            self.listOfAsteroids.pop(0)
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
-#Asteroid1 = SmallAsteroid(200,300,4,math.pi / 4)
-Asteroids = spawnAsteroids(6,gameDisplay,2) # initiating a list of asteroids to keep track of
-BlackScreen = pygame.image.load('BlackScreen.jpg') # black screen is the background
-gameDisplay.blit(BlackScreen,(0,0))
-for j in Asteroids:
-    j.update(gameDisplay) # testing the many asteroids spawn in the right spot
+clock = pygame.time.Clock()
+CollAster = CollectionOfAsteroids(gameDisplay,2)
+CollAster.spawnAsteroids(6) # initiating a list of asteroids to keep track of
+Black = (0,0,0) # black screen is the background
+gameDisplay.fill(Black)
+CollAster.updateAsteroids(gameDisplay) # testing the many asteroids spawn in the right spot
 pygame.display.update()
-pygame.time.delay(3000)
-for i in range(400):
-    gameDisplay.blit(BlackScreen,(0,0))
-    #Asteroid1.update(gameDisplay)
-    for j in Asteroids:
-        j.update(gameDisplay) # update each asteroid
-    pygame.display.update()
-    pygame.time.delay(20)
-for k in Asteroids:
-    Asteroids += k.destroy() # destroying all of the asteroids making them medium
-for i in range(400):
-    gameDisplay.blit(BlackScreen,(0,0))
-    #Asteroid1.update(gameDisplay)
-    for j in Asteroids:
-        j.update(gameDisplay) # reupdate everything
-    pygame.display.update()
-    pygame.time.delay(20)
+
 running = True # for the exit of the game
 while running:
+    gameDisplay.fill(Black)
+    CollAster.updateAsteroids(gameDisplay)# update each asteroid
+    pygame.display.update()
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_q:
+                running = False
 pygame.quit()
