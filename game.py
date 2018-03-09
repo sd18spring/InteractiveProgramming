4a26d9545ed3dfea82f57d358bbadd867ecc0ee1
brycemann8334 <bryce.mann@students.olin.edu>
2018-03-09 13:23:26 -0500

    added more classes/descriptions and prelim images

--- game.py
+++ game.py
@@ -11,9 +11,9 @@ class Model(object):
     """keeps track of the game state"""
     def __init__(self, environmentobjs, road, player):
         #initialize array that road will cycle through
-        self.arena_array = numpy.zeros(3,3,3)
+        self.arena_array = numpy.zeros(1,1,1)
         #environment objects will be tethered to a position on the road below
-        self.road = road
+        self.road = Road
         self.player = Player()
 
 class EnvironmentObjects(object):
@@ -36,14 +36,16 @@ class Obstacles(EnvironmentObjects):
 class Road():
     """describing the surface that will bring objects to the player"""
     def __init__(self, pedestrians, gastanks, obstacles):
-        self.pedestrians = Pedestrians
-        self.gastanks = Gastanks
-        self.obstacles = Obstacles
+        self.pedestrians = Pedestrians()
+        self.gastanks = Gastanks()
+        self.obstacles = Obstacles()
 
 class Player():
     """user controlled player"""
-    def __init__(self, position=(0,0)):
+    def __init__(self, position=(0,0), gas_level = 100, start_score=0):
         self.position = position
+        self.gas_level = gas_level
+        self.score = start_score
 
 class View():
     """drawing what is in the model"""
@@ -54,16 +56,33 @@ class Controllers():
     """keyboard controls"""
     def __init__(self, model):
         self.model = model
+        self.player = Player()
     #def __init__(self, )
     #def move_right
     #def move_left
     #def move_up
 
+class Menu():
+    """base class for main and pause menus"""
+    pass
+
+class MainMenu(Menu):
+    """main menu"""
+    pass
+
+class PauseMenu(Menu):
+    """pause menu"""
+    pass
+
+class Hud():
+    """display current score and gas level"""
+    def __init__(self):
+        self.player = Player()
 
 if __name__ == "__main__":
     size = (640, 480)
     screen = pygame.display.set_mode(size)
-
+    pygame.display.set_caption('ROAD RAGE')
     running = True
     while running:
         for event in pygame.event.get():
