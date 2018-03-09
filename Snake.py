6abfa4027f2e05664d37773432ea48b38330c4c6
junwonlee5 <junwon.lee@students.olin.edu>
2018-03-09 00:22:48 -0500

    Snake Project

--- Snake.py
+++ Snake.py
@@ -2,21 +2,43 @@ import pygame
 from pygame.locals import *
 import time
 
-class SnakeGame(object):
-    def __init__(self, width, height):
-        pygame.init()
-        self.width = width
-        self.height = height
-        self.screen = pygame.display.set_mode((self.width, self.height))
 
-    def mainloop(self):
+class SnakeGameWindowView(object):
+    def __init__(self, model, width, height):
+        self.model = model
+        self.screen = pygame.display.set_mode((width, height))
+    def draw(self):
+        self.screen.fill(pygame.Color(0, 0, 0))
+        for material in self.model.Mouse:
+            pygame.draw.rect(self.screen,
+                            pygame.Color(255, 0, 0),
+                            pygame.Rect(material.x, material.y, material.height, material.width))
+        for material in self.model.Snake:
+            pygame.draw.rect(self.screen,
+                            pygame.Color(0, 255, 0),
+                            pygame.Rect(material.x, material.y, material.height, material.width))
+        pygame.display.update()
 
-        run = True
-        while run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-        pygame.quit()
+class SnakeGameModel(object):
+
+    def __init__(self):
+        self.Mouse = []
+        self.Snake = []
+        snake = Snake(100, 10, 0, 0)
+        self.Mouse.append(Mouse(10, 10, 100, 100))
+        for i in range(int(snake.height / snake.width)):
+            self.Snake.append(Snake(snake.height / snake.width, 10, i*snake.width, 0))
+    def update(self):
+        for part in self.Snake:
+            part.update()
+    def __str__(self):
+        output_lines = []
+        for p in self.Mouse:
+            output_lines.append(str(p))
+        for p in self.Snake:
+            output_lines.append(str(p))
+        return "\n".join(output_lines)
+
 class Mouse(object):
 
     def __init__(self, height, width, x, y):
@@ -28,6 +50,85 @@ class Mouse(object):
     def __str__(self):
         return 'Mouse is in %f, %f' % (self.x, self.y)
 
+class Snake(object):
+
+    def __init__(self, height, width, x, y):
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+        self.vx = 0.0
+        self.vy = 0.0
+
+    def update(self):
+        self.x += self.vx
+        self.y += self.vy
+    def __str__(self):
+        return 'Snake is in %f, %f and is %f long.' % (self.x, self.y, self.height)
+
+class SnakeController(object):
+
+    def __init__(self, model):
+        self.model = model
+
+    def handle_event(self, event):
+        if event.type != KEYDOWN:
+            return
+        if event.key == pygame.K_DOWN:
+            while self.model.Snake[0].x != self.model.Snake[-1].x:
+                for i in range(len(self.model.Snake)-1):
+                    self.model.Snake[i].x += self.model.Snake[i+1].x - self.model.Snake[i].x
+                    if self.model.Snake[i].x == self.model.Snake[-1].x:
+                        self.model.Snake[i].y += self.model.Snake[i].width
+            if self.model.Snake[0].x == self.model.Snake[-1].x:
+                for part in self.model.Snake:
+                    part.vy = .1
+                    part.vx = 0
+
+        if event.key == pygame.K_LEFT:
+            while self.model.Snake[0].y != self.model.Snake[-1].y:
+                for i in range(len(self.model.Snake)-1):
+                    self.model.Snake[i].y -= self.model.Snake[i+1].y - self.model.Snake[i].y
+                    if self.model.Snake[i].y == self.model.Snake[-1].y:
+                        self.model.Snake[i].x -= self.model.Snake[i].width
+            if self.model.Snake[0].y == self.model.Snake[-1].y:
+                for part in self.model.Snake:
+                    part.vy = 0
+                    part.vx = -.1
+
+        if event.key == pygame.K_RIGHT:
+            while self.model.Snake[0].y != self.model.Snake[-1].y:
+                for i in range(len(self.model.Snake)-1):
+                    self.model.Snake[i].y += self.model.Snake[i+1].y - self.model.Snake[i].y
+                    if self.model.Snake[i].y == self.model.Snake[-1].y:
+                        self.model.Snake[i].x += self.model.Snake[i].width
+            if self.model.Snake[0].y == self.model.Snake[-1].y:
+                for part in self.model.Snake:
+                    part.vy = 0
+                    part.vx = .1
+        if event.key == pygame.K_UP:
+            while self.model.Snake[0].x != self.model.Snake[-1].x:
+                for i in range(len(self.model.Snake)-1):
+                    self.model.Snake[i].x += self.model.Snake[i+1].x - self.model.Snake[i].x
+                    if self.model.Snake[i].x == self.model.Snake[-1].x:
+                        self.model.Snake[i].y -= self.model.Snake[i].width
+            if self.model.Snake[0].x == self.model.Snake[-1].x:
+                for part in self.model.Snake:
+                    part.vx = 0
+                    part.vy = -.1
 if __name__ == '__main__':
-    window = SnakeGame(640, 800)
-    window.mainloop()
+    pygame.init()
+    model = SnakeGameModel()
+    window = SnakeGameWindowView(model, 640, 800)
+    run = True
+    control = SnakeController(model)
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            control.handle_event(event)
+        model.update()
+        window.draw()
+        time.sleep(0.001)
+    pygame.quit()
