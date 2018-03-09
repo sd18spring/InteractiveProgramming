f4cc57e9424a6c3d566dabb906c4036c681e504c
Tweir195 <tommy.weir@students.olin.edu>
2018-03-09 15:06:02 -0500

    Updated fingerTrack to be in line with paint.py

--- fingerTrack.py
+++ fingerTrack.py
@@ -12,9 +12,10 @@ class finger_track():
         self.cx = 0
         self.cy = 0
         self.path = []
+        self.clearpath = []
         self.red_maskL = [np.array([0, 150, 100]), np.array([178, 150, 100])]
-        self.red_maskH = [np.array([2, 255, 255]), np.array([180, 255, 255])]
-        self.refreshDelay = 3
+        self.red_maskH = [np.array([1, 255, 255]), np.array([180, 255, 255])]
+        self.refreshDelay = 0
 
     def BGR2HSV(self, frame):
         """This functions takes in a frame and converts it from BGR to BGR2HSV
@@ -39,26 +40,28 @@ class finger_track():
                 self.cy = int(M['m01'] / M['m00'])
                 self.frame_num = 0
                 self.path.append((self.cx, self.cy))
-                # if diffx > 100 or diffy > 100:
-                #     print('too far')
-                # else:
             cv2.circle(target, (self.cx, self.cy), 2, (0, 255, 0), -1)
         except IndexError:
             """"""
 
     def draw(self, target):
+        if len(self.path) == 1:
+            clearpath.append(path[0])
+        elif len(self.path) > 2:
+            pair = self.path[-2]
+            # print(pair, pair[0], cx, pair[1], cy)
+            diffx = abs(self.cx-pair[0])
+            diffy = abs(self.cy-pair[1])
+            distance = math.sqrt(diffx**2+diffy**2)
+            if distance<10:
+                self.clearpath.append(pair)
         for i in range(len(self.path)):
             # TODO: Add if statements to make sure that any outliers
             # would be removed from the list or ignored when drawing
             # the linewidth
-
-            # diffx = math.fabs(self.cx-self.path[-1][0])
-            # print(diffx)
-            # diffy = math.fabs(cy-path[-1][1])
-            # print(diffy)
-            if len(self.path) == 1:
-                break
+            # if len(self.clearpath) < 1:
+            #     break
             # elif math.sqrt(diffx**2+diffy**2) > 30:
                 # break
-            elif i < (len(self.path)-1):
-                cv2.line(target, self.path[i], self.path[i+1], (255, 0, 0), 3)
+            if i < (len(self.clearpath)-1):
+                cv2.line(target, self.clearpath[i], self.path[i+1], (255, 0, 0), 3)
