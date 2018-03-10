362b882bbf3a4d0a7c98236a81c03d7eb34620f1
brycemann8334 <bryce.mann@students.olin.edu>
2018-03-10 12:41:17 -0500

    road matrix

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 from pygame.font import *
 import time
 import numpy
-
+import random
 pygame.init()
 
 
@@ -11,34 +11,58 @@ class Model(object):
     """keeps track of the game state"""
     def __init__(self, environmentobjs, road, player):
         #initialize array that road will cycle through
-        self.arena_array = numpy.zeros(1,1,1)
+        self.arena_array = numpy.zeros((10,10,10))
         #environment objects will be tethered to a position on the road below
-        self.road = Road
+        self.road = Road()
         self.player = Player()
 
-class EnvironmentObjects():
+    def add_road(self):
+        self.arena_array[:,:,10] = road.add_obj(4)
+        return self.arena_array
+
+class EnvironmentObject():
     """base class for objects"""
     def __init__(self):
         pass
 
-class Gastanks(EnvironmentObjects):
-    """describing type of EnvironmentObject"""
-    pass
 
-class Pedestrians(EnvironmentObjects):
+class Gastanks(EnvironmentObject):
     """describing type of EnvironmentObject"""
-    pass
+    def __init__(self):
+        pass
+    def __str__(self):
+        return '1'
 
-class Obstacles(EnvironmentObjects):
+class Pedestrians(EnvironmentObject):
     """describing type of EnvironmentObject"""
-    pass
+    def __init__(self):
+        pass
+    def __str__(self):
+        return '2'
+
+class Obstacles(EnvironmentObject):
+    """describing type of EnvironmentObject"""
+    def __init(self):
+        pass
+    def __str__(self):
+        return '3'
 
 class Road():
     """describing the surface that will bring objects to the player"""
-    def __init__(self, pedestrians, gastanks, obstacles):
-        self.pedestrians = Pedestrians()
-        self.gastanks = Gastanks()
-        self.obstacles = Obstacles()
+    def __init__(self):
+        self.road_matrix = numpy.zeros((10,10),dtype=numpy.object_)
+
+    def __str__(self):
+        return str(self.road_matrix)
+
+    def add_obj(self, x_pos):
+        gas = Gastanks()
+        ped = Pedestrians()
+        obst = Obstacles
+        obj_list = [gas, ped, obst]
+        obj = obj_list[random.randint(0,2)]
+        self.road_matrix[9, x_pos] = obj
+        return self.road_matrix
 
 class Player():
     """user controlled player"""
@@ -46,6 +70,8 @@ class Player():
         self.position = position
         self.gas_level = gas_level
         self.score = start_score
+    def __str__(self):
+        return '4'
 
 class View():
     """drawing what is in the model"""
@@ -80,17 +106,29 @@ class Hud():
         self.player = Player()
 
 if __name__ == "__main__":
-    size = (640, 480)
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('ROAD RAGE')
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-        time.sleep(.001)
+    road = Road()
+    road.add_obj(1)
+    print(road)
 
-    pygame.quit()
+
+
+
+
+
+
+
+
+    #size = (640, 480)
+    #screen = pygame.display.set_mode(size)
+    #pygame.display.set_caption('ROAD RAGE')
+    #running = True
+    #while running:
+        #for event in pygame.event.get():
+            #if event.type == QUIT:
+                #running = False
+        #time.sleep(.001)
+
+    #pygame.quit()
 
 
 #testing
