1131d06d0212e88f8a0561aea72e8d36c63bc587
Enmo Ren <enmo.ren@students.olin.edu>
2018-03-11 04:25:05 -0400

    Finish Bomb User class, working on Lives.update

--- Main4.py
+++ Main4.py
@@ -16,7 +16,6 @@ class PyGameWindowView(object):
         self.screen = pygame.display.set_mode(size)
         self.bombflag = True
 
-
     def draw(self):
         """ Draw the current game state to the screen """
         self.screen.fill(pygame.Color(255, 255, 255))
@@ -35,6 +34,12 @@ class PyGameWindowView(object):
         self.player = pygame.transform.scale(pygame.image.load('Genie1.png'),(100,100))
         self.screen.blit(self.player, (self.model.player.center_x, self.model.player.center_y))
 
+        self.skeleton = pygame.transform.scale(
+            pygame.image.load('skeleton.jpg'), (60, 60))
+
+        for skeleton in self.model.skeletons:
+            self.screen.blit(self.skeleton, (skeleton.left, skeleton.top))
+
         pygame.display.update()
 
 class Model(object):
@@ -45,17 +50,44 @@ class Model(object):
         self.bomb_moving_sped = 8
         self.bomb = Bomb(random.randrange(0,self.width), self.bomb_init_height, self.bomb_init_height, self.height, self.width, self.bomb_moving_sped)
 
-        self.player = User(10,10,self.width)
+        self.player_init_position = 400
+        self.player_y_position = 500
+        self.player = User(self.player_init_position, self.player_y_position,self.width)
+
+        self.skeletons = []
+        self.lives = 3
+        self.skeleton_top = 50
+        self.skeleton_left = 50
+        self.skeleton_space = 40
+
+        # if 460 <= self.bomb.center_y <= 540 and self.player.center_x - 50 <= self.bomb.center_x <= self.player.center_x + 100:
+        #     self.lives -= 1
+        #     # print ("1")
+        #
+        # for x in range(self.skeleton_left,
+        #                self.skeleton_left * self.lives,
+        #                self.skeleton_space):
+        #     # print ("2")
+        #     self.skeletons.append(Lives(x, self.skeleton_top))
 
     def update(self):
         self.bomb.update()
         self.player.update()
 
+        if 460 <= self.bomb.center_y <= 540 and self.player.center_x - 50 <= self.bomb.center_x <= self.player.center_x + 100:
+            self.lives -= 1
+            self.bomb.center_y = self.width + 100
+
+        for x in range(self.skeleton_left,
+                       self.skeleton_left * self.lives,
+                       self.skeleton_space):
+            self.skeletons.append(Lives(x, self.skeleton_top))
+
 
 class Bomb(object):
-    """ Represents a bird in dodging game """
+    """ Represents a bomb in dodging game """
     def __init__(self, center_x, center_y, start_y, display_width, display_height, moving_speed):
-        """ Create a ball object with the specified geometry """
+        """ Create a bomb object with the specified geometry """
         self.center_x = center_x
         self.center_y = center_y
         self.start_y = start_y
@@ -64,7 +96,6 @@ class Bomb(object):
         self.moving_speed = moving_speed
 
     def update(self):
-
         # update the position of bomb each time
         self.center_y += self.moving_speed
         # if the bomb achieve the bottom of the screen
@@ -73,45 +104,59 @@ class Bomb(object):
             self.center_x = random.randrange(0, self.display_width)
 
 class User(object):
-    """ Represents the user in my dodging game """
+    """ Represents the player in dodging game """
 
     def __init__(self, center_x, center_y, display_width):
         """ Create a ball object with the specified geometry """
         self.center_x = center_x
         self.center_y = center_y
         self.display_width = display_width
+        self.center_x = self.display_width * 0.5 - 50
 
     def update(self):
         self.ret, frame = cap.read()
         self.faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20, 20))
-        self.center_x = self.display_width * 0.5
+        # self.center_x = self.display_width * 0.5 - 50
         for (x, y, w, h) in self.faces:
             cv2.circle(frame, (w / 2 + x, h / 2 + y), 10, (255, 255, 255), -1)
-        # self.center_x = x
+            self.center_x = (float)(x) / (444) * self.display_width
+            # print (x)
+
+        # Hardcoded cord for out of bounds center_x(Need change!!!!)
+        if self.center_x >700:
+            self.center_x =700
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pygame.quit()
+            cap.release()
+            cv2.destroyAllWindows()
 
 
-class Heart(object):
-    '''represents the number of lives that the user has left. The player always starts with 3 lives'''
+class Lives(object):
+    '''The number of lives the player has. Starts with 3 lives.'''
 
-    def __init__(self, left, top , width, height):
+    def __init__(self, left, top ):
         self.left = left
         self.top = top
-        self.width = width
-        self.height = height
+        self.lives = 3
+
+    # def update(self):
+
 
 if __name__ == '__main__':
-    pygame.init()
-    size = (800, 600)
-    model = Model(size)
-    view = PyGameWindowView(model, size)
 
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     kernel = np.ones((40, 21), 'uint8')
     cap = cv2.VideoCapture(0)
+
+    pygame.init()
+    size = (800, 600)
+    model = Model(size)
+    view = PyGameWindowView(model, size)
 
     running = True
     while running:
         model.update()
         view.draw()
         time.sleep(.001)
-    pygame.quit()
