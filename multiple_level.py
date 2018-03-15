c4eb70767877a3f0742f88bb3d56933ea2c2c60d
AllisonBusa <allison.busa@students.olin.edu>
2018-03-15 10:09:14 -0400

    adding horse following carrot improved

--- multiple_level.py
+++ multiple_level.py
@@ -3,17 +3,22 @@ from pygame.locals import *
 import time
 from turtle import *
 import os
+from random import *
+import random as random
+
 dog = pygame.image.load('dog2.jpg')
 space = pygame.image.load('space.png')
 wave = pygame.image.load('wave.jpg')
 sky = pygame.image.load('sky.jpg')
+mountain = pygame.image.load('mountain.jpg')
 gameover = pygame.image.load('gameover.jpg')
 youwin = pygame.image.load('win.jpg')
-horsey = pygame.image.load('horsey.jpg')
-carrot = pygame.image.load('carrot.jpg')
-from random import *
-import random as random
-
+horsey = pygame.image.load('horsey2.jpg')
+horsey.set_colorkey((255,255,255))
+carrot = pygame.image.load('carrot2.png')
+carrot.set_colorkey((255,255,255))
+horsey_rect = horsey.get_rect()
+carrot_rect = carrot.get_rect()
 
 backgrounds = [dog,space, wave,sky]
 class PyGameWindowView(object):
@@ -67,9 +72,9 @@ class Obstacle(object):
         self.image = pygame.Surface((50, 50))
         self.image.fill((150, 60, 10))
         self.pos = pygame.math.Vector2(random.randrange(1230),
-                                   random.randrange(750))
-        self.vel = pygame.math.Vector2(random.uniform(-5, 5),
-                                   random.uniform(-5, 5))
+                                   random.randrange(910))
+        self.vel = pygame.math.Vector2(random.uniform(-10, 10),
+                                   random.uniform(-10, 10))
 
     def __str__(self):
         return "Obstacle height=%f, width=%f, x=%f, y=%f" % (self.height,
@@ -77,17 +82,32 @@ class Obstacle(object):
                                                           self.x,
                                                           self.y)
 
+    def update(self):
+        self.pos += self.vel
+        if self.pos[0] < 50:
+            #self.pos[0] += 50
+            self.vel = (random.randint(1, 10), random.randint(1,10))
+        if self.pos[0] > 1230:
+            #self.pos[0] -= 50
+            self.vel = (random.randint(-10, -1), random.randint(-10,-1))
+        if self.pos[1] < 50:
+            #self.pos[1] += 50
+            self.vel = (random.randint(1, 10), random.randint(1,10))
+        if self.pos[1] > 750:
+            #self.pos[1] -= 50
+            self.vel = (random.randint(-10, -1), random.randint(-10,-1))
 
 def create_newLevel(PyGameWindowView, Target, background, size):
     """This function creates a whole new background with different
     obstacles for the next level."""
     obstacles = []
-    for i in range(random.randint(0,15)):
+    for i in range(random.randint(0,10)):
         obstacle = Obstacle()
         obstacles.append(obstacle)
     nextLevel = PyGameWindowView(target,size)
     nextLevel.screen.blit(background, (0,0))
     for obstacle in obstacles:
+        obstacle.update()
         nextLevel.screen.blit(obstacle.image, obstacle.pos)
 
     pygame.display.update()
@@ -117,13 +137,11 @@ if __name__ == '__main__':
             mousex, mousey = pygame.mouse.get_pos()
             pygame.mouse.set_visible(False)
 
-            view.screen.blit(view.background,(0,0))
-            view.screen.blit(carrot, (mousex, mousey))
-            view.screen.blit(horsey, (mousex-500, mousey))
+
 
             ### Collisions ###
             for obstacle in obstacles:
-                if mousex in range(int(obstacle.pos[0]), int(obstacle.pos[0]+50)) and mousey in range(int(obstacle.pos[1]), int(obstacle.pos[1]+50)):
+                if range(horsey_rect.left, horsey_rect.right) in range(int(obstacle.pos[0]), int(obstacle.pos[0]+50)) and range(horsey_rect.bottom, horsey_rect.top) in range(int(obstacle.pos[1]), int(obstacle.pos[1]+50)):
                     view.screen.blit(gameover, (0,0))
 
             ###Leveling Up ###
@@ -139,7 +157,12 @@ if __name__ == '__main__':
                 pygame.mouse.set_pos((0,480)) #resetting mouse position to the left most part of the screen
             if score == 5:
                 view.screen.blit(youwin, (0,0))
-
+        view.screen.blit(view.background,(0,0))
+        view.screen.blit(carrot, (mousex+30, mousey+30))
+        view.screen.blit(horsey, (mousex-500, mousey))
+        for obstacle in obstacles:
+            obstacle.update()
+            view.screen.blit(obstacle.image, obstacle.pos)
         pygame.display.flip()
         view.draw()
         pygame.display.update()
