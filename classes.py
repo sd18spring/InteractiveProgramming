594b778d610dd9397790b135d7af16de108b2e8e
ASHSWAN1999 <ashley.swanson@students.olin.edu>
2018-03-16 02:22:38 -0400

    Finished documentation

--- classes.py
+++ classes.py
@@ -4,11 +4,15 @@ from pygame.locals import *
 import pyaudio
 from pyaudio import *
 import wave
+import time
 
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile # get the api
 from numpy import arange
+
+import pickle
+from pickle import dump, load
 
 SCREENWIDTH = 1500
 SCREENHEIGHT = 1000
@@ -62,39 +66,35 @@ class Player(pygame.sprite.Sprite):
             elif self.rect.y > 0:
                 self.rect.y -= self.increment
 
-    def collide(self, obstacles, score):
+    def collide(self, obstacles, game):
         if pygame.sprite.spritecollide(self, obstacles, False):
-            print(int(score))
+            f = open('stop.txt', 'wb')
+            insert = pickle.dumps('stop')
+            f.write(insert)
+            f.close()
+
+            pygame.font.init()
+            myfont = pygame.font.SysFont('Comic Sans MS', 300)
+            textsurface = myfont.render("Score=%d"%(int(game.score)), False, (255, 255, 255))
+            game.mainSurface.blit(textsurface,(275,400))
+            pygame.display.update()
+
+            time.sleep(2)
             pygame.quit()
             sys.exit()
 
-    def record(self, record_time, a_format=pyaudio.paInt16, channels=2, rate=44100, chunk=1024):
-        self.is_recording = True
-        audio = pyaudio.PyAudio()
-        stream = audio.open(format=a_format, channels=channels,
-                        rate=rate, input=True,
-                        frames_per_buffer=chunk)
-
-        frames = []
-        for i in range(0, int(rate / chunk * record_time)):
-            data = stream.read(chunk)
-            frames.append(data)
-
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
-
-        waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        waveFile.setnchannels(channels)
-        waveFile.setsampwidth(audio.get_sample_size(a_format))
-        waveFile.setframerate(rate)
-        waveFile.writeframes(b''.join(frames))
-        waveFile.close()
-        self.is_recording = False
+    def analyze_freq(self):
 
 
-    def analyze_freq(self, filename='file.wav'):
-        fs, data = wavfile.read(filename) # load the data
+        try:
+            fs, data = wavfile.read('file.wav') # load the data
+        except:
+            data = self.data
+            fs = self.fs
+
+        self.data = data
+        self.fs = fs
+
         a = data.T[0] # this is a two channel soundtrack, I get the first track
         b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
         c = fft(b) # calculate fourier transform (complex numbers list)
@@ -108,6 +108,7 @@ class Player(pygame.sprite.Sprite):
 
         val = max(abs(c[2:(d-1)]))
         i = list(abs(c[2:(d-1)])).index(val)
+        #print(frqLabel[2:(d-1)][i])
         return frqLabel[2:(d-1)][i]
 
     def remap_interval(self, val,
@@ -124,13 +125,24 @@ class Player(pygame.sprite.Sprite):
         return new_val
 
     def freq_movement(self, frequency):
-        frequencyish = self.remap_interval(frequency, self.calibrated_high, self.calibrated_low)
-        if frequencyish > self.rect.y:
-            self.rect.y -=20
-        elif frequencyish < 50:
+        mid_freq = (self.calibrated_low + self.calibrated_high)/2
+        if frequency < 70:
             pass
-        elif frequencyish < self.rect.y:
-            self.rect.y += 20
+        #elif frequency > 1000:
+        #    pass
+
+
+        elif frequency > mid_freq:
+            if self.rect.y < self.increment:
+                self.rect.y = 0
+            elif self.rect.y > 0:
+                self.rect.y -= self.increment
+
+        elif frequency < mid_freq:
+            if self.rect.y > SCREENHEIGHT-(self.size + self.increment):
+                self.rect.y = SCREENHEIGHT - self.size
+            elif self.rect.y < SCREENHEIGHT- self.size:
+                self.rect.y += self.increment
 
 
 
@@ -139,6 +151,9 @@ class Player(pygame.sprite.Sprite):
 class Game:
     def __init__(self, num_blocks, player_color=(255, 0, 0)):
         pygame.init()
+        #pygame.font.init() # you have to call this at the start,
+                   # if you want to use this module.
+
 
         self.mainSurface = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT), 0, 32)
         pygame.display.set_caption("Collision Game")
@@ -155,10 +170,11 @@ class Game:
             myBlock.rect.y = random.randrange(SCREENHEIGHT - 150)
             self.blocksGroup.add(myBlock)
 
-        self.player = Player(self.mainSurface, player_color, 50, 20)
+        self.player = Player(self.mainSurface, player_color, 50, 10)
         self.player.rect.x = 200
         self.player.rect.y = 0.5*SCREENHEIGHT
         self.player_group.add(self.player)
+        self.myfont = pygame.font.SysFont('Comic Sans MS', 150)
 
     def run(self):
         while True:
@@ -168,16 +184,17 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 self.player.on_event(event)
-            # if self.player.is_recording == False:
-            #     self.player.record(0.2)
             freq = self.player.analyze_freq()
             self.player.freq_movement(freq)
             self.mainSurface.fill((0, 0, 0))
+            textsurface = self.myfont.render(str(int(self.score)), False, (255, 255, 255))
+            print(textsurface)
 
-            self.player.collide(self.blocksGroup, self.score)
+            self.player.collide(self.blocksGroup, self)
 
             self.player_group.draw(self.mainSurface)
             self.blocksGroup.update()
             self.blocksGroup.draw(self.mainSurface)
+            self.mainSurface.blit(textsurface,(10,10))
             pygame.display.update()
-            self.score += .01
+            self.score += .1
--- record.py
+++ record.py
@@ -1,9 +1,14 @@
 import pyaudio
 from pyaudio import *
 import wave
+import os
+from os.path import exists
+import sys
+import pickle
+from pickle import dump, load
 
 
-def record(record_time, a_format=pyaudio.paInt16, channels=2, rate=44100, chunk=1024):
+def record(record_time, a_format=pyaudio.paInt16, channels=2, rate=44100, chunk=1024, file_name='file.wav'):
     audio = pyaudio.PyAudio()
     stream = audio.open(format=a_format, channels=channels,
                     rate=rate, input=True,
@@ -18,7 +23,7 @@ def record(record_time, a_format=pyaudio.paInt16, channels=2, rate=44100, chunk=
     stream.close()
     audio.terminate()
 
-    waveFile = wave.open('file.wav', 'wb')
+    waveFile = wave.open(file_name, 'wb')
     waveFile.setnchannels(channels)
     waveFile.setsampwidth(audio.get_sample_size(a_format))
     waveFile.setframerate(rate)
@@ -27,6 +32,20 @@ def record(record_time, a_format=pyaudio.paInt16, channels=2, rate=44100, chunk=
 
 
 
+f = open('stop.txt', 'wb')
+insert = pickle.dumps('go')
+f.write(insert)
+f.close()
 
+while True:
+    f = open('stop.txt', 'rb+')
+    curr = f.read()
+    #print("curr = ", curr)
+    f.seek(0, 0)
+    update = pickle.loads(curr)
+    #print("update = ", update)
 
-record(1)
+    if update=='stop':
+        #print(update)
+        break
+    record(0.2)
