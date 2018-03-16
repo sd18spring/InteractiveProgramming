241821e7f0d4d42bdfea21d5d1be5c0251a164e4
Rachel Won <rachel.won@students.olin.edu>
2018-03-16 01:56:18 -0400

    Reconstructed code to put into MVC structure

--- rachel.py
+++ rachel.py
@@ -18,14 +18,10 @@ THINGS TO DO:
     b. easy, medium, hard mode (single player)
     x. rotate screen
 """
-class Penguin(pygame.sprite.Sprite): # code is from pygame documenta
-    # Constructor. Pass in the color of the block,
-    # and its x and y position
+class Penguin(pygame.sprite.Sprite):
     def __init__(self):
         # Call the parent class (Sprite) constructor
         super().__init__()
-
-        # Create an image of the block, and fill it with a color
         self.image = pygame.image.load("penguin_smol.png")
         self.rect = self.image.get_rect()
     def moveUp(self, pixels):
@@ -39,8 +35,6 @@ class Penguin(pygame.sprite.Sprite): # code is from pygame documenta
             self.rect.y = 340
         else:
             self.rect.y += pixels
-
-
 
 class Obstacles(pygame.sprite.Sprite):
     def __init__(self, image_name, rect = None):
@@ -60,18 +54,49 @@ class Obstacles(pygame.sprite.Sprite):
 class Powerups(Obstacles):
     def speedUp(self, pixels = 7):
         self.rect.x -= pixels
-# class Model:
-#     def __init__(self):
-#         self.all_penguins = pygame.sprite.Group()
-#         penguin = Penguin()
 
-class Sled_Main:
-    def __init__(self):
+class FinishLine(Obstacles):
+    pass
+
+class KeyController(object):
+    def __init__(self, model):
+        self.model = model
+
+    def event_type(self, keys, state):
+        if keys[pygame.K_LEFT] and state == 1:
+            self.model.penguin.moveUp(0)
+        if keys[pygame.K_RIGHT] and state == 1:
+            self.model.penguin.moveDown(0)
+        if keys[pygame.K_LEFT] and state == 2:
+            self.model.penguin.moveUp(5)
+        if keys[pygame.K_RIGHT] and state == 2:
+            self.model.penguin.moveDown(5)
+
+class CPSledView(object):
+    def __init__(self, model, width, height):
+        self.model = model
+        size = (width, height) #viewing frame is 500 x 400 pixels
+        self.model.WHITE = pygame.Color(255, 255, 255)
+        self.model.screen = pygame.display.set_mode(size)
+        #self.model.pygame.display.set_caption("Club Penguing Sledding Game")
+
+    def draw(self, model, timer):
+        self.model = model
+        self.model.screen.blit(timer, (425, 20))
+        self.model.boulders.draw(self.model.screen)
+        self.model.ice_patches.draw(self.model.screen)
+        self.model.all_penguins.draw(self.model.screen)
+        self.model.finish_line_group.draw(self.model.screen)
+        pygame.display.update()
+
+class CPSledModel(object):
+    def __init__(self, width, height):
         pygame.init()
-        #track length is 4000 x 400
-        size = (500, 400) #viewing frame is 500 x 400 pixels
+        size = (width, height) #viewing frame is 500 x 400 pixels
         self.WHITE = pygame.Color(255, 255, 255)
         self.screen = pygame.display.set_mode(size)
+        self.height = height
+        self.width = width
 
     def loadSprites(self):
         self.penguin = Penguin()
@@ -79,6 +104,7 @@ class Sled_Main:
         self.all_penguins = pygame.sprite.RenderPlain(self.penguin)
         self.boulders = pygame.sprite.RenderPlain()
         self.ice_patches = pygame.sprite.RenderPlain()
+        self.finish_line_group = pygame.sprite.RenderPlain()
         for num_boulders in range(20):
             x_position = (num_boulders+1) * 400 #randint(260, 360)
             y_boulders = randint(1,3)
@@ -94,70 +120,67 @@ class Sled_Main:
             self.ice_patch = Powerups("ice_patch_flat.png", pygame.Rect(x_position, y_position, 280, 70))
             self.ice_patches.add(self.ice_patch)
 
-    def main_loop(self):
-        self.loadSprites()
-        list_of_obstacles = self.boulders.sprites()
-        list_of_obstacles.extend(self.ice_patches.sprites())
+        self.finish_line = FinishLine("finish_line.png", pygame.Rect(6000, -2, 144, 404))
+        self.finish_line_group.add(self.finish_line)
 
+    def update(self):
+        self.all_penguins.update()
+        self.boulders.update()
+        self.ice_patches.update()
+        self.screen.fill(self.WHITE)
+
+if __name__ == '__main__':
+
+    def main_loop():
+        pygame.init()
+        model = CPSledModel(500,400)
+        view = CPSledView(model, 500, 400)
+        controller = KeyController(model)
+        model.loadSprites()
+        list_of_obstacles = model.boulders.sprites()
+        list_of_obstacles.extend(model.ice_patches.sprites())
+        list_of_obstacles.append(model.finish_line)
+
+        font = pygame.font.Font(None, 32)
+        pygame.display.set_caption("Club Penguin Sledding Game")
         running = True
-        pygame.display.set_caption("Club Penguing Sledding Game")
         while running:
-            #self._redraw()
+            current_time = str(pygame.time.get_ticks()/1000)
+            timer = font.render(current_time, True, (0,0,0))
             for event in pygame.event.get():
                 if event.type is pygame.QUIT:
                     running = False
-            keys = pygame.key.get_pressed()
+
+            if model.penguin.rect.left > model.finish_line.rect.left+120:
+                break
 
             hit = False
             ice = False
+            finish = False
             for obstacle in list_of_obstacles:
-                if self.penguin.rect.colliderect(obstacle.rect):
+                if model.penguin.rect.colliderect(obstacle.rect):
                     hit = True
                     if type(obstacle) == Powerups:
                         ice = True
+                    elif type(obstacle) == FinishLine:
+                        finish = True
             for obstacle in list_of_obstacles:
-                if ice:
+                if ice or finish:
                     obstacle.speedUp()
                 elif hit:
                     obstacle.slowDown()
                 else:
                     obstacle.moveLeft()
-            if hit and not ice:
-                self.penguin.image = pygame.image.load("fallen_penguin_smol_demanding_diego.png")
-                if keys[pygame.K_LEFT]:
-                    self.penguin.moveUp(0)
-                if keys[pygame.K_RIGHT]:
-                    self.penguin.moveDown(0)
+            if hit and not ice and not finish:
+                model.penguin.image = pygame.image.load("fallen_penguin_smol_demanding_diego.png")
+                controller.event_type(pygame.key.get_pressed(), 1)
             else:
-                self.penguin.image = pygame.image.load("penguin_smol.png")
-                if keys[pygame.K_LEFT]:
-                    self.penguin.moveUp(5)
-                if keys[pygame.K_RIGHT]:
-                    self.penguin.moveDown(5)
-            self.all_penguins.update()
-            self.boulders.update()
-            self.ice_patches.update()
-            self.screen.fill(self.WHITE)
-            self.boulders.draw(self.screen)
-            self.ice_patches.draw(self.screen)
-            self.all_penguins.draw(self.screen)
-            pygame.display.update()
-            clock.tick(60)
+                model.penguin.image = pygame.image.load("penguin_smol.png")
+                controller.event_type(pygame.key.get_pressed(), 2)
+
+            model.update()
+            view.draw(model, timer)
+            clock.tick_busy_loop(60)
         pygame.quit()
 
-
-
-
-if __name__ == '__main__':
-    game = Sled_Main()
-    game.main_loop()
-
-
-    # running = True
-    # while running:
-    #     for event in pygame.event.get():
-    #         if event.type == QUIT:
-    #             running = False
-    #     time.sleep(.001)
-    #
-    # pygame.quit()
+    main_loop()
