29ce4ae12fcd3805ff7c244eb69134692c3b2d29
dberny <diego.berny@students.olin.edu>
2018-03-16 01:30:09 -0400

    disgust code

--- CPSled.py
+++ CPSled.py
@@ -41,7 +41,6 @@ class Penguin(pygame.sprite.Sprite): # code is from pygame documentation
             self.rect.y += pixels
 
 
-
 class Obstacles(pygame.sprite.Sprite):
     def __init__(self, image_name, rect = None):
         super().__init__()
@@ -61,6 +60,8 @@ class Powerups(Obstacles):
     def speedUp(self, pixels = 7):
         self.rect.x -= pixels
 
+class FinishLine(Obstacles):
+    pass
 
 class Sled_Main:
     def __init__(self):
@@ -76,12 +77,13 @@ class Sled_Main:
         self.all_penguins = pygame.sprite.RenderPlain(self.penguin)
         self.boulders = pygame.sprite.RenderPlain()
         self.ice_patches = pygame.sprite.RenderPlain()
+        self.finish_line_group = pygame.sprite.RenderPlain()
         for num_boulders in range(20):
             x_position = (num_boulders+1) * 400 #randint(260, 360)
             y_boulders = randint(1,3)
             for i in range(y_boulders):
                 y_position = randrange(0, 340, 70)
-                self.boulder = Obstacles("croproc.png", pygame.Rect(x_position, y_position, 60, 60))
+                self.boulder = Obstacles("croproc.png", pygame.Rect(x_position, y_position, 50, 50))
                 #self.boulder.rect = self.boulder.rect.move(35,105)
                 #self.boulder.rect.inflate_ip(0, -10)
                 #self.boulder.rect.inflate_ip(-20, -10)
@@ -93,11 +95,14 @@ class Sled_Main:
             self.ice_patch = Powerups("ice_patch_flat.png", pygame.Rect(x_position, y_position, 280, 70))
             self.ice_patches.add(self.ice_patch)
 
+        self.finish_line = FinishLine("finish_line.png", pygame.Rect(1000, -2, 144, 404))
+        self.finish_line_group.add(self.finish_line)
+
     def main_loop(self):
         self.loadSprites()
         list_of_obstacles = self.boulders.sprites()
         list_of_obstacles.extend(self.ice_patches.sprites())
-
+        list_of_obstacles.append(self.finish_line)
         #pygame.font()
         font = pygame.font.Font(None, 32)
 
@@ -113,8 +118,12 @@ class Sled_Main:
                     running = False
             keys = pygame.key.get_pressed()
 
+            if self.penguin.rect.left > self.finish_line.rect.left+120:
+                break
+
             hit = False
             ice = False
+            finish = False
             for obstacle in list_of_obstacles:
                 if self.penguin.rect.colliderect(obstacle.rect):
                     #print(self.penguin.rect.left)
@@ -122,14 +131,16 @@ class Sled_Main:
                     hit = True
                     if type(obstacle) == Powerups:
                         ice = True
+                    elif type(obstacle) == FinishLine:
+                        finish = True
             for obstacle in list_of_obstacles:
-                if ice:
+                if ice or finish:
                     obstacle.speedUp()
                 elif hit:
                     obstacle.slowDown()
                 else:
                     obstacle.moveLeft()
-            if hit and not ice:
+            if hit and not ice and not finish:
                 self.penguin.image = pygame.image.load("fallen_penguin_smol_demanding_diego.png")
                 if keys[pygame.K_LEFT]:
                     self.penguin.moveUp(0)
@@ -150,6 +161,7 @@ class Sled_Main:
             self.boulders.draw(self.screen)
             self.ice_patches.draw(self.screen)
             self.all_penguins.draw(self.screen)
+            self.finish_line_group.draw(self.screen)
             pygame.display.update()
             clock.tick_busy_loop(60)
         pygame.quit()
