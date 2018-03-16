2a7134159f7b80226f4df06f94605d5304071a37
cusaitech <cusaitech@gmail.com>
2018-03-15 23:04:53 -0400

    Add a welcome screem

--- SnakeWithBoundary.py
+++ SnakeWithBoundary.py
@@ -12,28 +12,34 @@ class SnakeGameWindowView(object):
         self.font = pygame.font.SysFont("arial", 50)
         self.width = width
         self.height = height
-    def draw(self):
+    def draw1(self):
         """
         This function in the class will draw necessary materials
         and update every change in the window.
         """
+        if run:
+            self.screen.fill(pygame.Color(0, 0, 0))
+            for material in self.model.Mouse:
+                pygame.draw.rect(self.screen,
+                                pygame.Color(255, 0, 0),
+                                pygame.Rect(material.x, material.y, material.height, material.width))
+            for material in self.model.Snake:
+                pygame.draw.rect(self.screen,
+                                pygame.Color(0, 255, 0),
+                                pygame.Rect(material.x, material.y, material.height, material.width), not material.head)
+            for material in self.model.Arena:
+                pygame.draw.rect(self.screen,
+                                pygame.Color(0, 0, 255),
+                                pygame.Rect(material.x, material.y, material.width, material.height),
+                                material.padding)
+            play_score = self.font.render(str(self.model.Score.score), 1, (125, 125, 0))
+            self.screen.blit(play_score,(self.width - 100 , 50))
+            pygame.display.update()
 
+    def draw2(self):
         self.screen.fill(pygame.Color(0, 0, 0))
-        for material in self.model.Mouse:
-            pygame.draw.rect(self.screen,
-                            pygame.Color(255, 0, 0),
-                            pygame.Rect(material.x, material.y, material.height, material.width))
-        for material in self.model.Snake:
-            pygame.draw.rect(self.screen,
-                            pygame.Color(0, 255, 0),
-                            pygame.Rect(material.x, material.y, material.height, material.width), not material.head)
-        for material in self.model.Arena:
-            pygame.draw.rect(self.screen,
-                            pygame.Color(0, 0, 255),
-                            pygame.Rect(material.x, material.y, material.width, material.height),
-                            material.padding)
-        play_score = self.font.render(str(self.model.Score.score), 1, (125, 125, 0))
-        self.screen.blit(play_score,(self.width // 2 , self.height // 2))
+        welcome = self.font.render("Press Enter to Start", 1, (125, 125, 0))
+        self.screen.blit(welcome,(100 , self.height // 2))
         pygame.display.update()
 
 class SnakeGameModel(object):
@@ -60,10 +66,13 @@ class SnakeGameModel(object):
                 self.Snake[i].x = self.Snake[i+1].x
                 self.Snake[i].y = self.Snake[i+1].y
         self.Snake[-1].update()
+
         if self.Snake[-1].x == self.Mouse[0].x and self.Snake[-1].y == self.Mouse[0].y:
             self.Mouse[0].x = random.randint(2,60) * 10
             self.Mouse[0].y = random.randint(2, 70) * 10
             self.Score.update()
+
+            #Below is the snake growth logic
             if self.Snake[1].x - self.Snake[0].x == 10:
                 self.Snake.insert(0, Snake(10, 10, self.Snake[0].x-10, self.Snake[0].y))
             if self.Snake[1].x - self.Snake[0].x == -10:
@@ -72,8 +81,11 @@ class SnakeGameModel(object):
                 self.Snake.insert(0, Snake(10, 10, self.Snake[0].x, self.Snake[0].y-10))
             if self.Snake[1].y - self.Snake[0].y == -10:
                 self.Snake.insert(0, Snake(10, 10, self.Snake[0].x, self.Snake[0].y+10))
+
             print('The Snake gained length!')
-            print(self.Score)
+            print("Score: " + str(self.Score))
+
+        #Below is the snake collision logic
         if abs(self.Snake[-1].x - Arena().width) < Arena().padding or abs(self.Snake[-1].y - Arena().height) < Arena().padding:
             pygame.quit()
         if self.Snake[-1].x < Arena().padding - 10 or self.Snake[-1].y < Arena().padding - 10:
@@ -81,6 +93,7 @@ class SnakeGameModel(object):
         for i in range(len(self.Snake)-1):
             if self.Snake[i].x - self.Snake[-1].x == 0 and self.Snake[i].y - self.Snake[-1].y == 0:
                 pygame.quit()
+
     def __str__(self):
         output_lines = []
         for p in self.Mouse:
@@ -172,6 +185,7 @@ class SnakeController(object):
         """
         if event.type != KEYDOWN:
             return
+
         if event.key == pygame.K_DOWN:
             if abs(self.model.Snake[-1].vy) != 0:
                 return
@@ -205,15 +219,31 @@ if __name__ == '__main__':
     pygame.init()
     model = SnakeGameModel()
     window = SnakeGameWindowView(model, 640, 800)
-    run = True
+    run = False
     control = SnakeController(model)
+
+    while not run:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
+                    run = True
+                    break
+        model.update()
+        window.draw2()
+        time.sleep(0.1)
+
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                #run = False
+                break
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    #run = False
+                    break
             control.handle_event(event)
         model.update()
-        window.draw()
+        window.draw1()
         time.sleep(0.1)
     pygame.quit()
